7345d1b7202221f4d3443342e0960eb6a60e7333
dipwgt <dip@webgentechnologies.com>
2019-03-20 12:30:37 +0530

    first upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,52 @@
+import { Component, ViewChild } from '@angular/core';
+import { Nav, Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list';
+import { LoginPage } from "../pages/login/login";
+import {RegisterPage} from "../pages/register/register";
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  @ViewChild(Nav) nav: Nav;
+
+  rootPage: any = LoginPage;
+
+  pages: Array<{title: string, component: any, icon: string;}>;
+
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+    this.initializeApp();
+
+    // used for an example of ngFor and navigation
+    this.pages = [
+      { title: 'Home', component: HomePage, icon: 'home' },
+      { title: 'Register', component: RegisterPage, icon: 'home' },
+      { title: 'List', component: ListPage, icon: 'home' }
+    ];
+
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
+  }
+
+  logout() {
+    this.nav.setRoot(LoginPage);
+  }
+
+}
